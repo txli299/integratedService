d89e09afc3c04eb37c5e089fe414e7a6b0473f3d
CliffLi <txli299@gmail.com>
2023-11-07 19:34:23 -0600

    init repo

---
+++ src/main/java/com/gomobile/integratedService/IntegratedServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gomobile.integratedService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegratedServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegratedServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gomobile/integratedService/IntegratedServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gomobile.integratedService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegratedServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
